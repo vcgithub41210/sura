03580e10a355da7804da24cf4bb8fd74c795c546
vcgithub41210 <113379660+vcgithub41210@users.noreply.github.com>
2024-08-05 12:03:19 +0530

    swap cheythu

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 function getInputValue() {
-	// Get the input element by its ID
-	var inputElement = document.getElementById('myInput');
-	var documentImage = document.getElementById('myImage');
-	var displayElement = document.getElementById('display');
-	// Retrieve the value from the input element
-	var inputValue = inputElement.value;
-	if (inputValue == "sura"){
-		documentImage.src = 'images.jpeg';
-		documentImage.style.display = 'block';
-		displayElement.textContent = 'dont try to play the fool with me Niggesh'
-	}
-	else{
-		displayElement.textContent = 'Fuck off';
-		documentImage.style.display = 'none';
-	}
-	// Display the value in the <p> element with ID 'display'
-	// displayElement.textContent = 'Input value: ' + inputValue;
+        // Get the input element by its ID
+        var inputElement = document.getElementById('myInput');
+        var documentImage = document.getElementById('myImage');
+        var displayElement = document.getElementById('display');
+        // Retrieve the value from the input element
+        var inputValue = inputElement.value;
+        if (inputValue == "sura"){
+                documentImage.src = 'images.jpeg';
+                documentImage.style.display = 'block';
+                displayElement.textContent = 'dont try to play the fool with me Niggesh'
+        }
+        else{
+                documentImage.style.display = 'none';
+                displayElement.textContent = 'Fuck off';
+        }
+        // Display the value in the <p> element with ID 'display'
+        // displayElement.textContent = 'Input value: ' + inputValue;
 }
